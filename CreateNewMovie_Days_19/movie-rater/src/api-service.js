fc50f073a15e310944d8af2538b03b44809ca386
ritesh Singh <52201727+ritesh9840@users.noreply.github.com>
2023-09-17 12:57:20 +0545

    update_list

---
+++ CreateNewMovie_Days_19/movie-rater/src/api-service.js
@@ -0,0 +1,19 @@
+
+const TOKEN="67073e141a882bd3a89fffeb606077301f1b8fbf"
+export class API{
+
+    static updateMovie(movie_id,body){
+
+       return  fetch(`http://127.0.0.1:8000/api/movies/${movie_id}/`,{
+        method:'PUT',
+        headers:{
+          'Content-Type':'application/json',
+          'Authorization':`Token ${TOKEN}`
+        },
+        body:JSON.stringify(body)
+      }).then(resp=>resp.json())
+      
+
+    }
+
+}
---
+++ CreateNewMovie_Days_19/movie-rater/src/componenets/movie-form.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import { API } from '../api-service';
+
+
+
+function MovieForm(props) {
+   
+    const [title,setTitle]=useState(props.movie.title );
+    const [description,setDescription]=useState(props.movie.description);
+
+    const updateClicked=()=>{
+        console.log("update here");
+        API.updateMovie(props.movie.id,{title,description})
+            .then(resp =>props.upDatedMovie(resp))
+        .catch(error=>console.log(error))
+    }
+
+   
+    return (
+        <React.Fragment>
+            {
+                props.movie ? (
+                    <div>
+                        <label htmlFor ="title">Title</label>
+                        <input id="title" type="text" placeholder="title" value ={title} 
+                            onChange={evt=>setTitle(evt.target.value)}
+                        /> <br/>
+                        <label htmlFor ="description">Description</label>
+                        <textarea id="description" type="text" placeholder="description" value ={description} 
+                         onChange={evt=>setDescription(evt.target.value)}
+                        /> <br/>
+                        <button onClick={updateClicked}>Update</button>
+
+                    </div>
+                ) : null
+            }
+        </React.Fragment>
+
+    )
+}
+
+export default MovieForm
